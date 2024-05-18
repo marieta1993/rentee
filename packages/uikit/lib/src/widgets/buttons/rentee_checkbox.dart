51f99032ab7f57944a452011192e4b335c3106e3
Vahagn <dole2000@mail.ru>
2024-05-18 16:32:18 +0400

    check_box end

---
+++ packages/uikit/lib/src/widgets/buttons/rentee_checkbox.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:uikit/src/theme/rentee_border_styles.dart';
+import 'package:uikit/src/theme/rentee_colors.dart';
+
+class CheckBox extends StatefulWidget {
+  final bool checkValue;
+  final ValueChanged<bool?>? checkCallBack;
+  const CheckBox({
+    super.key,
+    required this.checkValue,
+    required this.checkCallBack,
+  });
+
+  @override
+  State<CheckBox> createState() => _CheckBoxState();
+}
+
+class _CheckBoxState extends State<CheckBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: widget.checkValue,
+      onChanged: widget.checkCallBack,
+      activeColor: RenteeColors.primary,
+      shape: RoundedRectangleBorder(borderRadius: borderRounded5),
+    );
+  }
+}
